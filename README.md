971250006a1be32a9383a90f1ff0fad3ebf177f9
wmccarth123 <bill7123@gmail.com>
2022-03-11 12:53:10 -0500

    updating rm and front end code

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Project-3
 
-## Project Proposal: 
+## Our Task: 
 
 We are a new project team within Enterprise rent a car tasked with trying exploring ways to utilize tokens in order to drive growth in our subscription service. Our strategy will be to create three classes of tokens signifying different levels of subscription for standard, upgraded, and premium. Then create a book of service offerings for each class of token. Finally, we are to develop a marketing strategy for these products within the Philadelphia test market.
+
+This analysis included the following steps:
+
+1. Create example Tokens for the three leves of subscripting 
+    * Code the XP_Token. XP_TokenCrowdsale, and XP_TokenCrowdsaleDeployer Contracts
+    * Compile and Deploy contracts Using Injected web3 & Meta Mask
+    * Copy the ABI of copiled contacts into a sperate .json file 
+    * copy the SMART_CONTRACT_ADDRESS and WEB3_PROVIDER_URI into a .env inorder to connect contracts to the front end completed in step 2 
+2. Display the tokens by creating a front end display using app.py file 
+    * Connect to XP_TokenCrowdsale by importing the SMART_CONTRACT_ADDRESS, WEB3_PROVIDER_URI, and .json file 
+    * Code front end display 
+    * Run Streamlit on computer terminal to display finalized from end 
+4. Analyze Pennsylvania Census Data to consider marketing plan for the tokenized subscription service.
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import json
 from tkinter import Image
+from turtle import color
 from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
@@ -10,6 +11,29 @@ from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
 
 load_dotenv()
 
+def set_background_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://blusignalsystems.com/wp-content/uploads/2016/12/Savin-NY-Website-Background-Web1.jpg");
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+set_background_url()
+
+#Title = '<p style="font-family:Courier; color:Purple; font-size: 20px;">Testing if this works</p>'
+#st.markdown(Title, unsafe_allow_html=True)
 # Define and connect a new Web3 provider
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
@@ -40,31 +64,70 @@ def load_contract():
 # Load the contract
 contract = load_contract()
 
-################################################################################
-# Helper functions to pin files and json to Pinata
-################################################################################
+#st.markdown('<p style=“color:blue">Enterprise Car Subscription Options</p>', unsafe_allow_html=True )
+from PIL import Image
 
-st.title("Buy Rental Tokens")
-st.write("Choose an account to get started")
-accounts = w3.eth.accounts
-address = st.selectbox("Select Account", options=accounts)
+image_4 = Image.open("logo-enterprise.jpeg")
+st.image(image_4, width=700)
+st.title("Enterprice Car Subscription")
+st.write ("Our car subscription service is the solution for those who want more flexibility than you get with car leasing. Instead of investing money in new cars that go down in value, save your money for vacations or other expenses. It’s also hard to know if you will want to drive the same car in 1, 2 or even 5 years, so it’s better to stay flexible. With our tokenized car subscription you can be a worry-free part of your life, just like your streaming service or a gym membership! With an all-inclusive car subscription service, you have mobility with no fuss at a fixed price. Get a car immediately and keep it for as long as you want!")
+
+st.title ("Subscription Benifits")
+st.write ("Call and reserve the vehicle you want from a range of sedans, SUVs and pickup trucks. Insurance, maintenance and roadside assistance are covered. Access to diverse, extensive fleet in 90+ countries and territories. Access to Emerald Aisle, Emerald Alerts, and status match. ")
+
+st.title ("How it Works")
+st.write ("Step 1. Reserve - Login, select the vehicle you want and how long you'll need it. With our wallet you can reserve vehicles on-the-go. Step 2. Unlock and Go - Hold your wallet token over the windshield sensor to unlock the vehicle, the keys will be waiting for you inside. Step 3. Return - Return the vehicle at the end of your reservation. Hold your wallet token over the reader one last time, the doors will lock and your rental has ended.")
 st.markdown("---")
 
 
-if st.button ("Buy Tokens"): 
-    #w3.eth.balanceOf(address)
+st.title("Input your Etherium Wallet Account")
+accounts = w3.eth.accounts
+address = st.selectbox("Input Account", options=accounts)
 
-    tx_hash = contract.functions.weiRaised()
-    #receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    st.write(dict(tx_hash))
+st.markdown("---")
+
+st.title("Economy")
+st.title("Price: 300 Eth")
+if st.button ("Detials of Package 1"):
+        st.markdown("The standard subscription service offered. Access to Compact Cars through Standard SUVs. No upgrades avalaible")  
+
+if st.button ("Purchase Package 1"):
+    st.markdown("Congradulations On Your Subscription!") 
+    st.balloons() 
 
 from PIL import Image
 
 image_1 = Image.open("car1.jpeg")
-image_2 = Image.open("car2.jpeg")
-image_3 = Image.open("car3.jpeg")
-
 st.image(image_1, width=400)
+
+st.markdown("---")  
+
+st.title("Full Size")
+st.title("Price: 500 Eth")
+if st.button ("Detials of Package 2"):
+        st.markdown("The Upgraded from tear 1 subscription offering services of...")  
+if st.button ("Purchase Package 2"):
+      st.markdown("Congradulations On Your Subscription!")
+      st.balloons()  
+    
+from PIL import Image
+
+image_2 = Image.open("car2.jpeg")
 st.image(image_2, width=400)
+
+st.markdown("---")  
+
+st.title("luxury")
+st.title("Price: 500 Eth")
+if st.button ("Detials of Package 3"):
+
+        st.markdown("ully comprehensive, insurance Re")  
+if st.button ("Purchase Package 3"):
+    st.markdown("Congradulations On Your Subscription!")  
+    st.balloons()
+
+from PIL import Image
+
+image_3 = Image.open("car3.jpeg")
 st.image(image_3, width=400)
 
